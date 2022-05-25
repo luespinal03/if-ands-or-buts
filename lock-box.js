37d0ec784f227a0df2d282db82c8a2e8d6d1df77
Luis Espinal <luise@MacBook-Pro.attlocal.net>
2022-05-25 10:41:16 -0700

    lockbox

--- lock-box.js
+++ lock-box.js
@@ -1,8 +1,11 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-let pincode = '1234'; 
+let pincode = "1234"; 
 
-let bouncer = prompt("Enter the pincode: ");
+let bouncer = prompt("Enter the pincode: "); // could change "prompt" for "Number"
+// if you change number for prompt then make sure the pincode is a number and not a string
+// number("Enter the pincode: ") = pincode = 1234;
+// prompt("Enter the pincode: ") = pincode = "1234";
 
 if(bouncer !== pincode){
     console.log("failure");
